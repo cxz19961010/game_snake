026e1e01e861657499a9ccc6a6e01b4a28e047d1
cxz19961010 <cxz19961010@163.com>
2016-07-08 02:08:49 +0800

    add a procedure which is for screen outputing

--- scr_output/scr_output.c
+++ scr_output/scr_output.c
@@ -26,21 +26,61 @@ int init(){
 	raw();
 	noecho();//取消回显
 	keypad(stdscr,TRUE);//支持特殊键位
+
 	main_win=newwin(height+2,width+2,0,0);
-	wborder(main_win,'|','|','-','-','+','+','+','+');
 	info_win=newwin(height+2,10,0,width+3);
+
+	wattrset(main_win,COLOR_PAIR(COLOR_BOX));
+	wattrset(info_win,COLOR_PAIR(COLOR_BOX));
+	wborder(main_win,'|','|','-','-','+','+','+','+');
 	wborder(info_win,'|','|','-','-','+','+','+','+');
+	wattroff(main_win,COLOR_PAIR(COLOR_BOX));
+	wattroff(info_win,COLOR_PAIR(COLOR_BOX));//绘制白色边线
+
+
+	refresh();
 	wrefresh(main_win);
 	wrefresh(info_win);
-	attroff(COLOR_PAIR(COLOR_BOX));
+	wattroff(main_win,COLOR_PAIR(COLOR_BOX));
+	wattroff(info_win,COLOR_PAIR(COLOR_BOX));
 }
 
-/*int print_snake(){
+int print_snake(){
+	wclear(main_win);
+	wclear(info_win);//清空屏幕
+
+	init_pair(COLOR_BOX,COLOR_WHITE,COLOR_BLACK);
 	init_pair(COLOR_SNAKE1,COLOR_RED,COLOR_RED);
 	init_pair(COLOR_SNAKE2,COLOR_BLUE);
+
+	wattrset(main_win,COLOR_PAIR(COLOR_BOX));
+	wattrset(info_win,COLOR_PAIR(COLOR_BOX));
+	wborder(main_win,'|','|','-','-','+','+','+','+');
+	wborder(info_win,'|','|','-','-','+','+','+','+');
+//绘制白色边线
+
+	wattrset(main_win,COLOR_PAIR(COLOR_SNAKE1)|A_REVERSE);
+	struct snake_node *node=player1.head;
+	while(node!=NULL)
+	{
+		mvwaddch(main_win,node->position[1],node->position[0],' ');
+		node=node->next;
+	}//绘制player1的图像
+
+	wattrset(main_win,COLOR_PAIR(COLOR_SNAKE2)|A_REVERSE);
+	node=player2.head;
+	while(node!=NULL)
+	{
+		mvwaddch(main_win,node->position[1],node->position[0],' ');
+		node=node->next;
+	}//绘制player2的图像
+
+	refresh();
+	wrefresh(main_win);
+	wrefresh(info_win);//输出两部分图像
+}
 	
-*/
-	 	
+
 
 int main(){
 	init();
