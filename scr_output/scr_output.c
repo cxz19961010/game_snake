9bd3ca7584afe9098503b290ca1889efd586485c
cxz19961010 <cxz19961010@163.com>
2016-07-07 15:56:05 +0800

    just saved for some functions' test
    and there arevery few differences

--- scr_output/scr_output.c
+++ scr_output/scr_output.c
@@ -1,11 +1,28 @@
 #include<stdio.h>
 #include<ncurses.h>
 
+#define COLOR_SNAKE1 1
+#define COLOR_SNAKE2 2
+#define COLOR_BOX 3
+struct map{
+	
+	struct{
+		int x,y;//记录地图的长宽
+		char **map;
+	};
+	int (*read_map)(void *Self,char *p);
+}map={10,5,NULL,read_map};
 
 WINDOW *main_win,*info_win;//main为游戏主界面，info为得分界面
-int init(int width,int height){
+int init(){
+	int height=map.y;
+	int	width=map.x;
 	initscr();
+	start_color();//初始化字体颜色
+
 	refresh();
+	init_pair(COLOR_BOX,COLOR_WHITE,COLOR_BLACK);
+	attron(COLOR_PAIR(COLOR_BOX));
 	raw();
 	noecho();//取消回显
 	keypad(stdscr,TRUE);//支持特殊键位
@@ -15,11 +32,18 @@ int init(int width,int height){
 	wborder(info_win,'|','|','-','-','+','+','+','+');
 	wrefresh(main_win);
 	wrefresh(info_win);
+	attroff(COLOR_PAIR(COLOR_BOX));
 }
 
-int print_snake()
+/*int print_snake(){
+	init_pair(COLOR_SNAKE1,COLOR_RED,COLOR_RED);
+	init_pair(COLOR_SNAKE2,COLOR_BLUE);
+	
+*/
+	 	
+
 int main(){
-	init(10,5);
+	init();
 	getch();	
 	endwin();
 
